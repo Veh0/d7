c8022e038c2ad04cfe72cf7daac466b0c0df5c35
veh0 <valentinohleyer@live.fr>
2019-10-31 18:03:12 +0100

    first commit

---
+++ web/sites/all/modules/custom/adimeo_weather.tpl.php
@@ -0,0 +1,5 @@
+<img src="<?php echo "http://openweathermap.org/img/wn/".$icon."@2x.png" ?>" alt="icon">
+<p><?php echo "Actually ".$location." is ".$weather ?></p>
+<p><b><?php echo $temperature ?></b></p>
+
+
